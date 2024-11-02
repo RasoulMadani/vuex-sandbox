00ff162fe138ba80695ddac2b9e31e984eebf1b4
Rasoulmadani <ali.rahbary@chmail.ir>
2024-11-02 15:53:13 +0330

    فیلتر کردن داده استیت

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,16 +3,29 @@
 export default {
 
   computed:{
+    doneTodos(){
+      return this.$store.state.todos.filter(todo=>{
+        if(todo.done === true){
+          return todo;
+        }
+      })
+    },
     todos(){
       return this.$store.state.todos
+      }
     }
-  }
+
 }
 </script>
 
 <template>
 <h4>home component</h4>
   <h5 v-for="todo in todos" :key="todo.id">
+    {{ todo.text}}
+
+  </h5>
+  Done Todo:
+  <h5 v-for="todo in doneTodos" :key="todo.id">
     {{ todo.text}}
 
   </h5>
