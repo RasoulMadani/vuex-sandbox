a5490892ab6a27c654357a1478c378a92bca9f57
Rasoulmadani <ali.rahbary@chmail.ir>
2024-11-02 16:46:55 +0330

    تغییر دادن استیت با میوتیشن

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,20 +5,15 @@ import {computed} from "vue";
 
 export default {
 
-  // computed:{
-  //   doneTodos(){
-  //     return this.$store.getters.doneTodos
-  //   },
-  //   todos(){
-  //     return this.$store.state.todos
-  //     }
-  //   }
   setup(){
     const store = useStore();
-    const doneTodos =  computed(()=>store.getters.doneTodos);
-    const getTodoById =  computed(()=>store.getters.getTodoById(1));
+    const count =  computed(()=>store.state.count);
+    function increment(){
+      store.commit("increment",2);
+    }
     return {
-      doneTodos,getTodoById
+      count,
+      increment
     }
   }
 }
@@ -26,16 +21,8 @@ export default {
 
 <template>
 <h4>home component</h4>
-<!--  <h5 v-for="todo in todos" :key="todo.id">-->
-<!--    {{ todo.text}}-->
-
-<!--  </h5>-->
-  Done Todo:
-  <h5 v-for="todo in doneTodos" :key="todo.id">
-    {{ todo.text}}
-
-  </h5>
-  <h1>{{getTodoById.id}}</h1>
+  <h5>{{count}}</h5>
+  <button @click="increment">Increment</button>
 </template>
 
 <style scoped>
--- src/store/index.js
+++ src/store/index.js
@@ -2,17 +2,11 @@ import {createStore} from "vuex";
 
 const store = createStore({
     state:{
-        todos:[
-            {id:1,text:'...1',done:false},
-            {id:2,text:'...2',done:true},
-        ]
+        count:1,
     },
-    getters:{
-        doneTodos(state){
-           return state.todos.filter(todo=>todo.done)
-        },
-        getTodoById :(state) => (id) =>{
-            return state.todos.find(todo=>todo.id== id);
+    mutations:{
+        increment(state,number){
+            state.count+=number;
         }
     }
 })
