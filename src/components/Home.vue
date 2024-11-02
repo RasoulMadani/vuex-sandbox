c19f72f9d17459adf62f26e0755b30323941fea3
Rasoulmadani <ali.rahbary@chmail.ir>
2024-11-02 14:03:08 +0330

    گرفتن مقدار متغیر از استور

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<script >
+export default {
+  computed:{
+    count(){
+      return this.$store.state.count;
+    }
+  }
+}
+</script>
+
+<template>
+<h4>home component</h4>
+  <h4>{{count}}</h4>
+</template>
+
+<style scoped>
+
+</style>
