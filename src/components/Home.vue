8cd49ee8591061df046759510da828ffb2c0dbea
Rasoulmadani <ali.rahbary@chmail.ir>
2024-11-02 16:36:22 +0330

    استفاده از گترها به سبکی دیگر

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,30 +1,41 @@
 <script >
 
+import {useStore} from "vuex";
+import {computed} from "vue";
+
 export default {
 
-  computed:{
-    doneTodos(){
-      return this.$store.getters.doneTodos
-    },
-    todos(){
-      return this.$store.state.todos
-      }
+  // computed:{
+  //   doneTodos(){
+  //     return this.$store.getters.doneTodos
+  //   },
+  //   todos(){
+  //     return this.$store.state.todos
+  //     }
+  //   }
+  setup(){
+    const store = useStore();
+    const doneTodos =  computed(()=>store.getters.doneTodos);
+    const getTodoById =  computed(()=>store.getters.getTodoById(1));
+    return {
+      doneTodos,getTodoById
     }
-
+  }
 }
 </script>
 
 <template>
 <h4>home component</h4>
-  <h5 v-for="todo in todos" :key="todo.id">
-    {{ todo.text}}
+<!--  <h5 v-for="todo in todos" :key="todo.id">-->
+<!--    {{ todo.text}}-->
 
-  </h5>
+<!--  </h5>-->
   Done Todo:
   <h5 v-for="todo in doneTodos" :key="todo.id">
     {{ todo.text}}
 
   </h5>
+  <h1>{{getTodoById.id}}</h1>
 </template>
 
 <style scoped>
--- src/store/index.js
+++ src/store/index.js
@@ -10,6 +10,9 @@ const store = createStore({
     getters:{
         doneTodos(state){
            return state.todos.filter(todo=>todo.done)
+        },
+        getTodoById :(state) => (id) =>{
+            return state.todos.find(todo=>todo.id== id);
         }
     }
 })
